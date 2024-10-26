c3b797f06b6d7c2d46048d6ec7f90c20d9eb03ec
Rahul Kumar <rr03597@gmail.com>
2024-10-26 09:16:43 +0530

    first commit

--- src/app/Service/item.service.ts
+++ src/app/Service/item.service.ts
@@ -5,37 +5,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ItemService {
-  //  private apiUrl= "http://localhost:3000/api/products";
-  // private postUrl = 'http://localhost:3000/api/product';
-  private postUrl= 'http://localhost:8080/api';
+  private postUrl = 'http://localhost:3000/api/product';
 
   constructor(private http: HttpClient) {}
 
   getProducts() {
-    // return this.http.get(this.postUrl);
-    return this.http.get(`${this.postUrl}/get/product`);
+    return this.http.get(this.postUrl);
   }
   createProducts(product: any) {
-    // return this.http.post(this.postUrl, product);
-    return this.http.post(`${this.postUrl}/create`, product);
+    return this.http.post(this.postUrl, product);
   }
 
   updateProduct(id: any, product: any) {
-    // return this.http.put(`${this.postUrl}/${id}`, product);
-    // return this.http.put(`${this.postUrl}/update/${id}`, product);
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.put(`${this.postUrl}/update/${id}`, product, { headers });
+    return this.http.put(`${this.postUrl}/${id}`, product);
   }
 
   deleteProduct(id: string) {
-    // return this.http.delete(`${this.postUrl}/${id}`);
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.delete(`${this.postUrl}/delete/product?id=${id}`, { headers });
-
+    return this.http.delete(`${this.postUrl}/${id}`);
   }
 
   getProductById(id: string) {
-    // return this.http.get(`${this.postUrl}/${id}`);
-    return this.http.get(`${this.postUrl}/get/product/${id}`);
+    return this.http.get(`${this.postUrl}/${id}`);
   }
 }
